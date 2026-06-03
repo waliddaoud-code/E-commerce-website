b56fc321455b667f59dcad0b909534847fa1ef5d
waliddaoud-code <daoud.walid.2002@gmail.com>
2026-06-03 03:14:18 +0100

    Set default base URL for axios requests

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,11 @@ import { BrowserRouter } from 'react-router'
 import './index.css'
 import App from './App.jsx'
 
+import axios from "axios";
+
+axios.defaults.baseURL =
+  import.meta.env.VITE_API_URL || "http://localhost:3000";
+
 
 
 createRoot(document.getElementById('root')).render(
